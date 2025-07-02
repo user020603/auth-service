bcd8f3cd74b1674657be11bff8084161f120b30c
user020603 <ptit.tienthanh@gmail.com>
2025-07-02 17:19:12 +0700

    refactor(auth/infrastructure): refactor database

--- infrastructure/database.go
+++ infrastructure/database.go
@@ -1,12 +1,13 @@
 package infrastructure
 
 import (
+	"context"
 	"fmt"
-	"thanhnt208/vcs-sms/auth-service/config"
 	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"thanhnt208/vcs-sms/auth-service/config"
 )
 
 type Database struct {
@@ -14,10 +15,34 @@ type Database struct {
 }
 
 func NewDatabase(cfg *config.Config) (IDatabase, error) {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=UTC",
-		cfg.DBHost, cfg.DBUser, cfg.DBPassword, cfg.DBName, cfg.DBPort)
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	dsn := buildDSN(cfg)
+	db, err := openGormDB(dsn, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open database: %w", err)
+	}
+	return &Database{db: db}, nil
+}
 
+func NewTestDatabase(mockDB *gorm.DB) IDatabase {
+	return &Database{db: mockDB}
+}
+
+func buildDSN(cfg *config.Config) string {
+	return fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=UTC",
+		cfg.DBHost,
+		cfg.DBUser,
+		cfg.DBPassword,
+		cfg.DBName,
+		cfg.DBPort,
+	)
+}
+
+func openGormDB(dsn string, gormCfg *gorm.Config) (*gorm.DB, error) {
+	if gormCfg == nil {
+		gormCfg = &gorm.Config{}
+	}
+	db, err := gorm.Open(postgres.Open(dsn), gormCfg)
 	if err != nil {
 		return nil, err
 	}
@@ -31,9 +56,7 @@ func NewDatabase(cfg *config.Config) (IDatabase, error) {
 	sqlDB.SetMaxOpenConns(100)
 	sqlDB.SetConnMaxLifetime(time.Hour)
 
-	return &Database{
-		db: db,
-	}, nil
+	return db, nil
 }
 
 func (d *Database) GetDB() *gorm.DB {
@@ -46,4 +69,12 @@ func (d *Database) Close() error {
 		return err
 	}
 	return sqlDB.Close()
+}
+
+func (d *Database) Ping(ctx context.Context) error {
+	sqlDB, err := d.db.DB()
+	if err != nil {
+		return err
+	}
+	return sqlDB.PingContext(ctx)
 }
